93491368d235604cd12f1a9e897534b71143db02
Evgeny <evgeniy.nakoskin@diginetica.com>
2024-06-24 16:10:38 +0400

    Добавлен калькулятор калорий на день и калькулятор калорий на 10 грамм пищи по БЖУ

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -23,10 +23,14 @@ namespace WpfApp2
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            if (weightIN.Text == "" || heightIN.Text == "") {
+                MessageBox.Show("Ошибка! Введите все свои данные");
+                return;
+            }
             double weight = Convert.ToDouble(weightIN.Text);
             double height = Convert.ToDouble(heightIN.Text);
-            double heightM = height / 100;
-            double index =  weight / (heightM*heightM);
+            double heightM = height;
+            double index =  (weight / (heightM*heightM)) *10000 ;
             int precision = 1;
             double roundIndex = Convert.ToDouble(index.ToString($"N{precision}"));
 
@@ -73,6 +77,80 @@ namespace WpfApp2
                 adviceOUT.Text = " Вам надо больше есть, у вас Выраженный дефицит массы тела";
 
             }
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        public double activityIndex;
+        private void btnBZUcalc_Click(object sender, RoutedEventArgs e)
+        {
+            if (weightIN.Text == "" || heightIN.Text == "" || ageIN.Text == "" || activityIndexComboBox.SelectedItem == null || genderIN.SelectedItem == null)
+            {
+                MessageBox.Show("Ошибка! Введите все свои данные");
+                return;
+            }
+            double weight = Convert.ToDouble(weightIN.Text);
+            double height = Convert.ToDouble(heightIN.Text);
+            double age = Convert.ToDouble(ageIN.Text);
+
+            if (activityIndexComboBox.SelectedItem == one_ActivityRange) { activityIndex = 1.2; }
+            if (activityIndexComboBox.SelectedItem == two_ActivityRange) { activityIndex = 1.375; }
+            if (activityIndexComboBox.SelectedItem == three_ActivityRange) { activityIndex = 1.46; }
+            if (activityIndexComboBox.SelectedItem == four_ActivityRange) { activityIndex = 1.55; }
+            if (activityIndexComboBox.SelectedItem == five_ActivityRange) { activityIndex = 1.64; }
+            if (activityIndexComboBox.SelectedItem == six_ActivityRange) { activityIndex = 1.72; }
+            if (activityIndexComboBox.SelectedItem == seven_ActivityRange) { activityIndex = 1.9; }
+
+            if (genderIN.SelectedItem == fgend) 
+            { 
+            double baseMet = 9.99 * weight + 6.25 * height - 4.92 * age - 161;
+                double kalByDay = baseMet * activityIndex;
+                bzuOut.Text = "Ваша норма:" + kalByDay.ToString() + "калорий/день";
+            }
+            else if (genderIN.SelectedItem == mgend)
+            {
+            double baseMet = 9.99 * weight + 6.25 * height - 4.92 * age +5;
+                double kalByDay = baseMet * activityIndex;
+                bzuOut.Text = "Ваша норма:" + kalByDay.ToString() + "калорий/день";
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            double B;
+            if (prodKal_Belki.Text == "")
+            {
+                B = 0;
+            }
+            else { 
+            B = Convert.ToDouble(prodKal_Belki.Text);
+            }
+
+            double Z;
+            if (prodKal_Ziri.Text == "" )
+            {
+                Z = 0;
+            }
+            else
+            {
+                Z = Convert.ToDouble(prodKal_Ziri.Text);
+            }
+
+            double U;
+            if (prodKal_Uglevodi.Text == "")
+            {
+                U = 0; 
+            }
+            else
+            {
+                U = Convert.ToDouble(prodKal_Uglevodi.Text);
+            }
+
+            double kal = (B * 4) + (Z * 4) + (U * 9);
+            productKalOUT.Text = "Калорийность: " + kal.ToString() + "калорий";
         }
     }
 }
